873c764d9ac8effa4dc96cc06e107d7c372c152c
TUNG PHAM <han.tae.w@gmail.com>
2018-10-08 18:33:06 +0700

    up

--- 2018-09-24/album/album.js
+++ 2018-09-24/album/album.js
@@ -1,7 +1,15 @@
-$(document).ready(function(){
-    $("button#bt_toggler").click(function(){
-        $("#navbarContent").slideToggle("slow","swing", function() {
-            // Animation complete.
-          })
-    });
-});
+// $(document).ready(function(){
+//     $("button#bt_toggler").click(function(){
+//         $("#navbarContent").slideToggle("slow","swing", function() {
+//             // Animation complete.
+//           })
+//     });
+// });
+function showhide(){
+    var x = document.getElementById("navbarContent");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
